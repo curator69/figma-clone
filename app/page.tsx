f580c08281909fa142beef537b2026181098d53f
Rushikesh <rushikeshkhillari10@gmail.com>
2024-02-18 16:15:57 +0530

    completed live cursors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div className="h-screen w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">something</h1>
+    <div>
+      <Live />
     </div>
   );
 }
